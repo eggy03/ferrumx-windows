44b64879f3f5716a121d856da1b30acc09376cb0
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-04 14:11:56 +0530

    docs(service): clarify `CommonMappingInterface` usage in Javadoc
    - Update Javadoc comments in `CommonServiceInterface` and `OptionalCommonServiceInterface` to clarify that `CommonMappingInterface` can be used via custom implementations or default methods for mapping results.

--- src/main/java/io/github/eggy03/ferrumx/windows/service/CommonServiceInterface.java
+++ src/main/java/io/github/eggy03/ferrumx/windows/service/CommonServiceInterface.java
@@ -12,7 +12,7 @@ import io.github.eggy03.ferrumx.windows.utility.TerminalUtility;
 import java.util.List;
 
 /**
- * Common service interface whose method implementations provide a way to fetch WMI data from Powershell
+ * Common service interface whose method implementations provide a way to fetch WMI data from PowerShell
  * in the form of a {@link List}
  * <p>
  *     Useful for implementing services of classes which return more than one instance
@@ -28,7 +28,8 @@ public interface CommonServiceInterface<S> {
     /**
      * Implementations of this method are expected to query the PowerShell using methods
      * that automatically handle the PowerShell process lifecycle
-     * and then map the results to the expected entity types using an implementation of {@link CommonMappingInterface}
+     * and then map the results to the expected entity types using a custom implementation or
+     * the default methods of {@link CommonMappingInterface}
      * @return a {@link List} of entities of type {@code <S>} defined by the caller
      * @since 2.2.0
      */
@@ -37,7 +38,8 @@ public interface CommonServiceInterface<S> {
     /**
      * Implementations of this method are expected to query the PowerShell using methods
      * that delegate the responsibility of managing the PowerShell session to the caller
-     * and then map the results to the expected entity types using an implementation of {@link CommonMappingInterface}
+     * and then map the results to the expected entity types using a custom implementation
+     * or the default methods of {@link CommonMappingInterface}
      * @param powerShell the caller-managed PowerShell session passed to the method
      * @return a {@link List} of entities of type {@code <S>} defined by the caller
      * @since 2.2.0
@@ -47,6 +49,8 @@ public interface CommonServiceInterface<S> {
     /**
      * Implementations of this method are expected to skip {@link PowerShell} entirely and rely on
      * {@link TerminalUtility} instead for PowerShell session management
+     * and then map the results to the expected entity types using a custom implementation
+     * or the default methods of {@link CommonMappingInterface}
      * @param timeout the maximum time (in seconds) to wait for the PowerShell command to complete before terminating the process
      * @return a {@link List} of entities of type {@code <S>} defined by the caller
      * @since 3.1.0
